a91b252b2a968abcb96817edeea2a38c14cfef43
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-12-04 00:09:04 -0300

    implement lock and purchase system

---
+++ backend/src/main/java/com/um/programacion2/trabajo_final/service/dto/SolicitudBloqueoDTO.java
@@ -0,0 +1,4 @@
+package com.um.programacion2.trabajo_final.service.dto;
+
+public class SolicitudBloqueoDTO {
+}
---
+++ backend/src/main/java/com/um/programacion2/trabajo_final/service/dto/catedra/BloqueoRequest.java
@@ -0,0 +1,4 @@
+package com.um.programacion2.trabajo_final.service.dto.catedra;
+
+public class BloqueoRequest {
+}
